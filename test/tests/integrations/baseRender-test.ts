e8485965ce6d71bf043aceab27de742196709f40
deris <305945915@qq.com>
2018-07-23 18:13:44 +0800

    add base dom render test

---
+++ test/tests/integrations/baseRender-test.ts
@@ -0,0 +1,34 @@
+import Simple from '../../../src/core/Simple'
+
+describe('Render Test', () => {
+    test('render base dom structure', () => {
+        let component = new Simple({
+            render(h) {
+                return h('div', {}, [
+                    `Welcome to Simple Js`
+                ])
+            }
+        })
+
+        let renderHtml = getRenderHtml()
+
+        // Set up our document body
+        document.body.innerHTML = `<div id="app"></div>`
+
+        Simple.mount('#app', component)
+
+        // This module has a side-effect
+
+        // Assert that the fetchCurrentUser function was called, and that the
+        // #username span's inner text was updated as we'd expect it to.
+        expect(getHtmlById('app')).toBe(renderHtml);
+    })
+})
+
+const getHtmlById = (id: string) => {
+    return document.getElementById(id).innerHTML.trim()
+}
+
+const getRenderHtml = () => {
+    return `<div>Welcome to Simple Js</div>`
+}
