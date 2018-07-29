c6cedd1f3d70f21a1b25246cbf717ec0f3140dbf
deris <305945915@qq.com>
2018-07-29 21:06:51 +0800

    directive

---
+++ src/statics/directievLifeCycle.ts
@@ -0,0 +1,24 @@
+/**
+ * native component lifecycle map
+ * */
+const lifeCycle: any = {
+    UNMOUNT: 'unmount',
+
+    BEFORE_CREATE: 'beforeCreate',
+
+    CREATED: 'created',
+
+    BEFORE_MOUNT: 'beforeMount',
+
+    MOUNTED: 'mounted',
+
+    BEFORE_UPDATE: 'beforeUpdate',
+
+    UPDATED: 'updated',
+
+    BEFORE_DESTROY: 'beforeDestroy',
+
+    DESTROYED: 'destroyed'
+}
+
+export default lifeCycle
