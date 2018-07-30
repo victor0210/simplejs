9b5b6db2ec927121cf6e09968aa868f124797a1c
deris <305945915@qq.com>
2018-07-30 21:34:02 +0800

    fix unit test

--- src/core/Simple.ts
+++ src/core/Simple.ts
@@ -4,13 +4,18 @@ import ComponentInjectMixins from "./ComponentInjectMixins";
 import ComponentProxy from "./ComponentProxy";
 import injectDirectives from "../directives/injectDirectives";
 
-function Simple (spec: any) {
+let hasInjected: boolean
+
+function Simple(spec: any) {
+    if (!hasInjected) {
+        injectDirectives()
+        hasInjected = true
+    }
+
     return new ComponentProxy(spec)
 }
 
 applyMixin(Simple, ComponentRendererMixins)
 applyMixin(Simple, ComponentInjectMixins)
-
-injectDirectives()
 
 export default Simple
